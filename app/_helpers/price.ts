b37bd3e537f34c0698ab785e9a780ccbfb61f608
maxsantos <maxsonsantos88@gmail.com>
2024-05-15 00:31:21 -0300

    feat: add product page and components

--- app/_helpers/price.ts
+++ app/_helpers/price.ts
@@ -4,8 +4,8 @@ export const calculateProductTotalPrice = (product: Product): number => {
   if (product.discountPercentage === 0) {
     return Number(product.price);
   }
-  const discount = Number(product.price) * (product.discountPercentage / 100);
 
+  const discount = Number(product.price) * (product.discountPercentage / 100);
   return Number(product.price) - discount;
 };
 
