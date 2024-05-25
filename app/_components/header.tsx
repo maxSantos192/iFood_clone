ab3e6df3f49cdc0f213b6f4c1239e658309a4950
maxsantos <maxsonsantos88@gmail.com>
2024-05-25 20:05:32 -0300

    refactor: add asChild on SheetTrigger button

--- app/_components/header.tsx
+++ app/_components/header.tsx
@@ -49,7 +49,7 @@ const Header = () => {
       </div>
 
       <Sheet>
-        <SheetTrigger>
+        <SheetTrigger asChild>
           <Button
             size="icon"
             variant="outline"
