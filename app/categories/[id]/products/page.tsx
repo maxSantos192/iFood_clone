b6c83af4f1aae2f3656d45ab5a968d12b021865f
Maxson Santos <maxsonsantos88@gmail.com>
2024-07-02 09:14:00 -0300

    feat: add RestaurantList in CategoriesPage

--- app/categories/[id]/products/page.tsx
+++ app/categories/[id]/products/page.tsx
@@ -1,4 +1,5 @@
 import ProductItem from "@/app/_components/product-item";
+import RestaurantList from "@/app/_components/restaurant-list";
 import { db } from "@/app/_lib/prisma";
 import { notFound } from "next/navigation";
 
@@ -15,10 +16,20 @@ const CategoriesPage = async ({ params: { id } }: CategoriesPageProps) => {
     },
     include: {
       products: {
+        take: 12,
         include: {
           restaurant: {
             select: {
               name: true,
+            },
+          },
+        },
+      },
+      restaurants: {
+        where: {
+          categories: {
+            some: {
+              id,
             },
           },
         },
@@ -32,9 +43,9 @@ const CategoriesPage = async ({ params: { id } }: CategoriesPageProps) => {
 
   return (
     <>
-      <div className="px-5 py-6">
-        <h2 className="mb-6 text-lg font-semibold">{category.name}</h2>
-        <div className="grid grid-cols-2 gap-6">
+      <div className="mx-auto w-full max-w-7xl py-6">
+        <h2 className="mb-6 px-5 text-lg font-semibold">{category.name}</h2>
+        <div className="grid grid-cols-2 gap-6 px-5 md:grid-cols-4 lg:grid-cols-6">
           {category?.products.map((product) => (
             <ProductItem
               key={category.id}
@@ -42,6 +53,10 @@ const CategoriesPage = async ({ params: { id } }: CategoriesPageProps) => {
               className="min-w-full"
             />
           ))}
+        </div>
+        <h2 className="px-5 py-6 text-lg font-semibold">Restaurantes</h2>
+        <div>
+          <RestaurantList />
         </div>
       </div>
     </>
